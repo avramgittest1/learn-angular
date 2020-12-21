c3c116318374a7743e9ad64776b14ea19d9d31bb
avramcristian <gittestsavramcristian1@gmail.com>
2020-12-21 16:52:10 +0200

    schimvare titul

--- angular101/src/app/app.component.ts
+++ angular101/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { ViewEncapsulation } from '@angular/compiler/src/core';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular101';
+  title = 'Real Madrid!!!';
 }
