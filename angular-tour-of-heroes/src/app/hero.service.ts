5254598d0d68c3ab8977e308bef544cc10c831d6
avramcristian <gittestsavramcristian1@gmail.com>
2020-12-23 12:59:00 +0200

    message service part 1

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -2,17 +2,19 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero.interface';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService : MessageService) { }
 
  
 
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService : Get featched heroes')
     return of(HEROES);
   }
 }
